4f1afe0276acf3523a18e63f89145bc6abd52cc6
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-05-17 18:20:58 +0200

    BugFix

--- js/listarandom.js
+++ js/listarandom.js
@@ -1,18 +1,19 @@
 function listarandom() {
-    var listaminimo, listamassimo, lista, listan;
-    listaminimo = parseInt(String(document.getElementById('listarandominmin').value));
-    listamassimo = parseInt(String(document.getElementById('listarandominmax').value));
-
-    lista = [];
-
-    while(lista.length < listamassimo) {
-        listan = Math.floor(Math.random() * (listamassimo - listaminimo) ) + listaminimo;
-         if(!lista.includes(listan)) {
-            lista.push(listan);
-        }
-    }
-
-    if (lista.length > 0) {
-        document.getElementById('listarandomout').innerHTML = lista;
-    }
+//    var listaminimo, listamassimo, lista, listan;
+//    listaminimo = parseInt(String(document.getElementById('listarandominmin').value));
+//    listamassimo = parseInt(String(document.getElementById('listarandominmax').value));
+//
+//    lista = [];
+//
+//    while(lista.length < listamassimo) {
+//        listan = Math.floor(Math.random() * (listamassimo - listaminimo) ) + listaminimo;
+//         if(!lista.includes(listan)) {
+//            lista.push(listan);
+//        }
+//    }
+//
+//    if (lista.length > 0) {
+//        document.getElementById('listarandomout').innerHTML = lista;
+//    }
+document.getElementById('listarandomout').innerHTML = "Funziona disabilitata";
 }
