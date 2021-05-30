09a2d510a78e50e6496b929f989a1656a16358c5
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-05-30 11:21:25 +0200

    ListUpdate

---
+++ js/list.js
@@ -0,0 +1,14 @@
+function target() {
+    var onoff = document.getElementById('link0').target;
+    if (onoff == "_self") {
+        for ( a = 0; a < 11; a++) {
+            var id = "link" + String(a);
+            document.getElementById(id).target="_blank";
+        }
+    } else if (onoff == "_blank") {
+        for ( a = 0; a < 11; a++) {
+            var id = "link" + String(a);
+            document.getElementById(id).target="_self";
+        }
+    }
+}
