c764e5f5dc4ce295955e6e88bab23be47a854abd
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-07-03 21:44:04 +0200

    SpecialThanksUpdate

--- js/specialthanks.js
+++ js/specialthanks.js
@@ -1,5 +1,5 @@
 var h = window.innerHeight;
 var h2 = h - 200;
 if( h2 > 700 ) {
-    document.getElementById('content').style = "height:" + h2 + "px"
+    document.getElementById('content').style = "height:" + h2 + "px";
 }
