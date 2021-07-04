0384329bda6a630336c8f48a76357f60018c0ae4
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-07-04 22:18:06 +0200

    SpecialThanksBugFix

--- js/specialthanks.js
+++ js/specialthanks.js
@@ -1,6 +1,8 @@
-var h = window.innerHeight;
-var minh = document.getElementById('content').style.minHeight;
-var h2 = h - 216;
-if( h2 > minh ) {
-    document.getElementById('content').style = "height:" + h2 + "px";
+if (!navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)) {
+    var h = window.innerHeight;
+    var minh = document.getElementById('content').style.minHeight;
+    var h2 = h - 216;
+    if( h2 > minh ) {
+        document.getElementById('content').style = "height:" + h2 + "px";
+    }
 }
