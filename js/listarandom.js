7fcf2b22f27f8179c1917731b41996d89c581422
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-11-20 21:35:38 +0100

    CalcoliUpdate

--- js/listarandom.js
+++ js/listarandom.js
@@ -1,19 +1,43 @@
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 function listarandom() {
-//    var listaminimo, listamassimo, lista, listan;
-//    listaminimo = parseInt(String(document.getElementById('listarandominmin').value));
-//    listamassimo = parseInt(String(document.getElementById('listarandominmax').value));
-//
-//    lista = [];
-//
+   var listaminimo, listamassimo, lista, listan;
+   listaminimo = parseInt(String(document.getElementById('listarandominmin').value));
+   listamassimo = parseInt(String(document.getElementById('listarandominmax').value));
+
+   lista = [];
+
+   for(a = listaminimo; a < listamassimo; a++) {
+       lista.push(a);
+   }
+
+   if (lista.length > 0) {
+    document.getElementById('listarandomout').innerHTML = shuffle(lista);
+    }
+
 //    while(lista.length < listamassimo) {
 //        listan = Math.floor(Math.random() * (listamassimo - listaminimo) ) + listaminimo;
 //         if(!lista.includes(listan)) {
 //            lista.push(listan);
 //        }
 //    }
-//
-//    if (lista.length > 0) {
-//        document.getElementById('listarandomout').innerHTML = lista;
-//    }
-document.getElementById('listarandomout').innerHTML = "Funziona disabilitata";
+
+   
+// document.getElementById('listarandomout').innerHTML = "Funziona disabilitata";
 }
