ca60ef88b083400fc54f5be36a760c83fdb444e1
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2021-11-22 17:14:41 +0100

    Aggiunta Pagina Programmazione

--- js/list.js
+++ js/list.js
@@ -1,12 +1,12 @@
 function target() {
     var onoff = document.getElementById('link0').target;
     if (onoff == "_self") {
-        for ( a = 0; a < 11; a++) {
+        for ( a = 0; a < 13; a++) {
             var id = "link" + String(a);
             document.getElementById(id).target="_blank";
         }
     } else if (onoff == "_blank") {
-        for ( a = 0; a < 11; a++) {
+        for ( a = 0; a < 13; a++) {
             var id = "link" + String(a);
             document.getElementById(id).target="_self";
         }
