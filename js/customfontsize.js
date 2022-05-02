e191d95e3ab0d26f031aced9738295c07b8cd605
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2022-05-02 14:39:53 +0200

    CustomFontSizeUpdate

--- js/customfontsize.js
+++ js/customfontsize.js
@@ -1,13 +1,25 @@
-function minus() {
+function minus(n) {
     var el = document.getElementById('txt');
     var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
     var fontSize = parseFloat(style);
-    el.style.fontSize = (fontSize - 1) + 'px';
+    el.style.fontSize = (fontSize - n) + 'px';
 }
 
-function plus() {
+function plus(n) {
     var el = document.getElementById('txt');
     var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
     var fontSize = parseFloat(style);
-    el.style.fontSize = (fontSize + 1) + 'px';
+    el.style.fontSize = (fontSize + n) + 'px';
 }
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Should do nothing if the default action has been cancelled
+    }
+
+    if (event.key == "+") {
+        plus(1);
+    } else if (event.key == "-") {
+        minus(1);
+    }
+  }, true);
